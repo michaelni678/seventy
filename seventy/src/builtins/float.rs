8b6c375b65e92149cb681b03c96a660561b43e3f
Michael Ni <michael.ni678@gmail.com>
2025-01-11 17:01:06 -0500

    docs: email and float doctests

--- seventy/src/builtins/float.rs
+++ seventy/src/builtins/float.rs
@@ -3,6 +3,23 @@
 use crate::core::Validator;
 
 /// [`Validator`] checks if finite.
+/// 
+/// # Examples
+///
+/// The example below validates the inner f32 is alphanumeric. Because of the
+/// newtype's guarantees, it is impossible to construct a `FiniteF32` with
+/// an inner f32 that is not finite.
+/// 
+/// ```
+/// use seventy::{builtins::float::*, core::Newtype, seventy};
+///
+/// #[seventy(validate(finite))]
+/// struct FiniteF32(f32);
+///
+/// assert!(FiniteF32::try_new(70.70).is_ok());
+/// assert!(FiniteF32::try_new(f32::INFINITY).is_err());
+/// assert!(FiniteF32::try_new(f32::NAN).is_err());
+/// ```
 pub struct finite;
 
 impl Validator<f32> for finite {
