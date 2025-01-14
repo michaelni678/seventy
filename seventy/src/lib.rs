d83f185262a50e6e6bdc89ded9084c9fd813debb
Michael Ni <michael.ni678@gmail.com>
2025-01-13 20:47:23 -0500

    docs: added more to root docs

--- seventy/src/lib.rs
+++ seventy/src/lib.rs
@@ -2,14 +2,15 @@
 
 //! # Seventy
 //!
-//! Seventy is a simple newtype sanitizer and validator.
+//! Seventy is a simple [newtype](https://doc.rust-lang.org/rust-by-example/generics/new_types.html) sanitizer and validator.
 //!
-//! The [`macro@seventy`] procedural macro is provided to automatically implement
-//! sanitization, validation, and other logic.
+//! The [`macro@seventy`] procedural macro is provided to automatically implement sanitization, validation, and other logic.
 //!
 //! ## Sanitizing
 //!
 //! Sanitization mutates a target. Sanitization is run before validation.
+//!
+//! ### Examples
 //!
 //! The example below sanitizes the inner string by trimming surrounding whitespace.
 //!
@@ -25,10 +26,41 @@
 //! );
 //! ```
 //!
+//! ### Custom Sanitizers
+//!
+//! Custom sanitizers can be defined with the [`Sanitizer`] trait.
+//!
+//! The sanitizer below divides the target by the given value.
+//!
+//! ```
+//! use std::ops::DivAssign;
+//!
+//! use seventy::{core::Sanitizer, seventy, Newtype};
+//!
+//! #[allow(non_snake_case)]
+//! pub struct divide_by<T>(pub T);
+//!
+//! impl<T> Sanitizer<T> for divide_by<T>
+//! where
+//!     T: DivAssign<T> + Copy,
+//! {
+//!     fn sanitize(&self, target: &mut T) {
+//!         target.div_assign(self.0);
+//!     }
+//! }
+//!
+//! #[seventy(sanitize(divide_by(5.0)))]
+//! pub struct DivideBy(f32);
+//!
+//! assert_eq!(DivideBy::try_new(10.0).unwrap().into_inner(), 2.0);
+//! ```
+//!
 //! ## Validating
 //!
 //! Validation checks if a target adheres to a set of rules. Validation
 //! is run after sanitization.
+//!
+//! ### Examples
 //!
 //! The example below validates the inner string is alphanumeric.
 //!
@@ -42,11 +74,44 @@
 //! assert!(Username::try_new("u$ername").is_err());
 //! ```
 //!
+//! ### Custom Validators
+//!
+//! Custom validators can be defined with the [`Validator`] trait.
+//!
+//! The validator below checks if the target is even.
+//!
+//! ```
+//! use seventy::{core::Validator, seventy, Newtype};
+//!
+//! #[allow(non_snake_case)]
+//! pub struct even_i64;
+//!
+//! impl Validator<i64> for even_i64 {
+//!     fn validate(&self, target: &i64) -> bool {
+//!         target.abs() % 2 == 0
+//!     }
+//! }
+//!
+//! #[seventy(validate(even_i64))]
+//! pub struct EvenI64(i64);
+//!
+//! assert!(EvenI64::try_new(2).is_ok());
+//! assert!(EvenI64::try_new(3).is_err());
+//! ```
+//!
+//! ### Errors?
+//!
+//! Seventy does not support error handling. A validator only returns a boolean indicating
+//! whether the validation result is valid or invalid. If you need to know the specific
+//! reason why a newtype couldn't be created, Seventy is not the crate for you.
+//!
 //! ## Upgrading
 //!
 //! Upgrades automatically implement useful functionality. More about upgrades
 //! and the different types of upgrades can be found in the documentation
 //! for the [`macro@seventy`] procedural macro.
+//!
+//! ### Examples
 //!
 //! The example below uses the `deref` upgrade, which implements `Deref` on
 //! the newtype.
@@ -63,6 +128,8 @@
 //!
 //! ## Incorporating it all
 //!
+//! ### Examples
+//!
 //! The example below first trims the target and then validates if the trimmed
 //! target is alphanumeric.
 //!
@@ -77,6 +144,9 @@
 //!
 //! assert!(Username::try_new("   u$ername   ").is_err());
 //! ```
+//!
+//! [`Sanitizer`]: seventy::core::Sanitizer
+//! [`Validator`]: seventy::core::Validator
 
 extern crate self as seventy;
 
