9117f9afb3fd34d5ff7269e857d6512dd51fbd4a
Michael Ni <michael.ni678@gmail.com>
2025-01-20 12:37:31 -0500

    test: generic_mixed_multiple

--- seventy-macros/tests/generics.rs
+++ seventy-macros/tests/generics.rs
@@ -7,7 +7,7 @@ fn generic_type_single() {
 
     assert_eq!(Generic::<i32>::try_new(70i32).unwrap().into_inner(), 70);
 
-    assert_eq!(Generic::<char>::try_new('c').unwrap().into_inner(), 'c');
+    assert_eq!(Generic::<char>::try_new('x').unwrap().into_inner(), 'x');
 
     assert_eq!(
         Generic::<&str>::try_new("value").unwrap().into_inner(),
@@ -40,8 +40,7 @@ fn generic_lifetime_single() {
     #[seventy(upgrades(independent))]
     pub struct Generic<'a>(&'a i32);
 
-    let value = 70;
-    assert_eq!(Generic::<'_>::try_new(&value).unwrap().into_inner(), &70);
+    assert_eq!(Generic::<'_>::try_new(&70).unwrap().into_inner(), &70);
 }
 
 #[test]
@@ -49,28 +48,31 @@ fn generic_lifetime_multiple() {
     #[seventy(upgrades(independent))]
     pub struct Generic<'a, 'b>(&'a &'b i32);
 
-    let value = 70;
-    let value_ref = &value;
+    assert_eq!(Generic::<'_, '_>::try_new(&&70).unwrap().into_inner(), &&70);
+}
+
+#[test]
+fn generic_mixed_single() {
+    #[seventy(upgrades(independent))]
+    pub struct Generic<'a, T>(&'a T);
+
+    assert_eq!(Generic::<'_, i32>::try_new(&70).unwrap().into_inner(), &70);
+
     assert_eq!(
-        Generic::<'_, '_>::try_new(&value_ref).unwrap().into_inner(),
-        &&70
+        Generic::<'_, &str>::try_new(&"value").unwrap().into_inner(),
+        &"value"
     );
 }
 
 #[test]
-fn generic_mixed() {
+fn generic_mixed_multiple() {
     #[seventy(upgrades(independent))]
-    pub struct Generic<'a, T>(&'a T);
+    pub struct Generic<'a, 'b, T, U>(&'a (T, &'b U));
 
-    let value = 70;
     assert_eq!(
-        Generic::<'_, i32>::try_new(&value).unwrap().into_inner(),
-        &70
-    );
-
-    let value = "value";
-    assert_eq!(
-        Generic::<'_, &str>::try_new(&value).unwrap().into_inner(),
-        &"value"
+        Generic::<'_, '_, f32, char>::try_new(&(70.70, &'x'))
+            .unwrap()
+            .into_inner(),
+        &(70.70, &'x')
     );
 }
