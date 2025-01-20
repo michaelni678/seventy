b74d0ea28a14d5bcd67cb3460cc768a4010af1cc
Michael Ni <michael.ni678@gmail.com>
2025-01-20 13:45:18 -0500

    test: construction count

--- seventy-macros/tests/seventy.rs
+++ seventy-macros/tests/seventy.rs
@@ -1,1 +1,100 @@
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use seventy::{
+    core::{Sanitizer, Validator},
+    seventy, Newtype,
+};
+
 mod upgrades;
+
+/// NOTE: This test is closely related to
+/// `upgrades::independent::sanitizer_construction_count`.
+#[test]
+fn sanitizer_construction_count() {
+    static SANITIZER_CONSTRUCTION_COUNT: AtomicUsize = AtomicUsize::new(0);
+    static SANITIZER_SANITIZE_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    #[allow(non_camel_case_types)]
+    pub struct s;
+
+    impl s {
+        pub fn new() -> Self {
+            let count = SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed);
+
+            SANITIZER_CONSTRUCTION_COUNT.store(count + 1, Ordering::Relaxed);
+
+            Self
+        }
+    }
+
+    impl Sanitizer<()> for s {
+        fn sanitize(&self, _target: &mut ()) {
+            let count = SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed);
+
+            SANITIZER_SANITIZE_COUNT.store(count + 1, Ordering::Relaxed);
+        }
+    }
+
+    #[seventy(sanitize(s::new()))]
+    pub struct Sanitized(());
+
+    assert_eq!(SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 0);
+    assert_eq!(SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed), 0);
+
+    let _ = Sanitized::try_new(());
+
+    assert_eq!(SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 1);
+    assert_eq!(SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed), 1);
+
+    let _ = Sanitized::try_new(());
+
+    assert_eq!(SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 1);
+    assert_eq!(SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed), 2);
+}
+
+/// NOTE: This test is closely related to
+/// `upgrades::independent::validator_construction_count`.
+#[test]
+fn validator_construction_count() {
+    static VALIDATOR_CONSTRUCTION_COUNT: AtomicUsize = AtomicUsize::new(0);
+    static VALIDATOR_VALIDATE_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    #[allow(non_camel_case_types)]
+    pub struct v;
+
+    impl v {
+        pub fn new() -> Self {
+            let count = VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed);
+
+            VALIDATOR_CONSTRUCTION_COUNT.store(count + 1, Ordering::Relaxed);
+
+            Self
+        }
+    }
+
+    impl Validator<()> for v {
+        fn validate(&self, _target: &()) -> bool {
+            let count = VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed);
+
+            VALIDATOR_VALIDATE_COUNT.store(count + 1, Ordering::Relaxed);
+
+            true
+        }
+    }
+
+    #[seventy(validate(v::new()))]
+    pub struct Validated(());
+
+    assert_eq!(VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 0);
+    assert_eq!(VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed), 0);
+
+    let _ = Validated::try_new(());
+
+    assert_eq!(VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 1);
+    assert_eq!(VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed), 1);
+
+    let _ = Validated::try_new(());
+
+    assert_eq!(VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 1);
+    assert_eq!(VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed), 2);
+}
--- seventy-macros/tests/upgrades/independent.rs
+++ seventy-macros/tests/upgrades/independent.rs
@@ -1,4 +1,9 @@
-use seventy::{seventy, Newtype};
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+use seventy::{
+    core::{Sanitizer, Validator},
+    seventy, Newtype,
+};
 
 #[test]
 fn generic_type_single() {
@@ -75,4 +80,96 @@ fn generic_mixed_multiple() {
             .into_inner(),
         &(70.70, &'x')
     );
+}
+
+/// NOTE: This test is closely related to `sanitizer_construction_count` in the
+/// test root.
+#[test]
+fn sanitizer_construction_count() {
+    static SANITIZER_CONSTRUCTION_COUNT: AtomicUsize = AtomicUsize::new(0);
+    static SANITIZER_SANITIZE_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    #[allow(non_camel_case_types)]
+    pub struct s;
+
+    impl s {
+        pub fn new() -> Self {
+            let count = SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed);
+
+            SANITIZER_CONSTRUCTION_COUNT.store(count + 1, Ordering::Relaxed);
+
+            Self
+        }
+    }
+
+    impl Sanitizer<()> for s {
+        fn sanitize(&self, _target: &mut ()) {
+            let count = SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed);
+
+            SANITIZER_SANITIZE_COUNT.store(count + 1, Ordering::Relaxed);
+        }
+    }
+
+    #[seventy(upgrades(independent), sanitize(s::new()))]
+    pub struct Sanitized(());
+
+    assert_eq!(SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 0);
+    assert_eq!(SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed), 0);
+
+    let _ = Sanitized::try_new(());
+
+    assert_eq!(SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 1);
+    assert_eq!(SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed), 1);
+
+    let _ = Sanitized::try_new(());
+
+    assert_eq!(SANITIZER_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 2);
+    assert_eq!(SANITIZER_SANITIZE_COUNT.load(Ordering::Relaxed), 2);
+}
+
+/// NOTE: This test is closely related to `validator_construction_count` in the
+/// test root.
+#[test]
+fn validator_construction_count() {
+    static VALIDATOR_CONSTRUCTION_COUNT: AtomicUsize = AtomicUsize::new(0);
+    static VALIDATOR_VALIDATE_COUNT: AtomicUsize = AtomicUsize::new(0);
+
+    #[allow(non_camel_case_types)]
+    pub struct v;
+
+    impl v {
+        pub fn new() -> Self {
+            let count = VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed);
+
+            VALIDATOR_CONSTRUCTION_COUNT.store(count + 1, Ordering::Relaxed);
+
+            Self
+        }
+    }
+
+    impl Validator<()> for v {
+        fn validate(&self, _target: &()) -> bool {
+            let count = VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed);
+
+            VALIDATOR_VALIDATE_COUNT.store(count + 1, Ordering::Relaxed);
+
+            true
+        }
+    }
+
+    #[seventy(upgrades(independent), validate(v::new()))]
+    pub struct Validated(());
+
+    assert_eq!(VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 0);
+    assert_eq!(VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed), 0);
+
+    let _ = Validated::try_new(());
+
+    assert_eq!(VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 1);
+    assert_eq!(VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed), 1);
+
+    let _ = Validated::try_new(());
+
+    assert_eq!(VALIDATOR_CONSTRUCTION_COUNT.load(Ordering::Relaxed), 2);
+    assert_eq!(VALIDATOR_VALIDATE_COUNT.load(Ordering::Relaxed), 2);
 }
